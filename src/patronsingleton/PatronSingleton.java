c8ec7847ecaef74f67056ec5943bb91853f05634
David Condori <davidcondori99@gmail.com>
2017-01-16 11:56:49 -0500

    primero comete

---
+++ src/patronsingleton/PatronSingleton.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package patronsingleton;
+
+/**
+ *
+ * @author Davidc
+ */
+public class PatronSingleton {
+
+    /**
+     * @param args the command line arMnguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Menu.getInstancia().Principal();
+        
+    }
+    
+}
